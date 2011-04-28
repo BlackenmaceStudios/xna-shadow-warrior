91ed1c4c06c54c519343a01b56f2cff7a097837f
justinmarshall20@gmail.com <justinmarshall20@gmail.com@ff800d11-3098-c92b-a8d1-3816c941664f>
2011-04-28 00:12:06 +0000

    Moved the editor to a separate class, buildlite is the developer project.

---
+++ trunk/Editor/EditorPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using build;
+namespace Editor
+{
+    public partial class EditorPage : UserControl
+    {
+        BuildEditor _editor;
+
+        public EditorPage()
+        {
+            InitializeComponent();
+        }
+
+        void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _editor = new BuildEditor();
+
+
+
+
+            System.Windows.Interop.SilverlightHost host = Application.Current.Host;
+            // The Settings object, which represents Web browser settings.
+            System.Windows.Interop.Settings settings = host.Settings;
+
+            // Read/write properties of the Settings object.
+            settings.EnableFrameRateCounter = true;
+            //        settings.EnableRedrawRegions = true;
+            settings.MaxFrameRate = 60;
+
+            _editor.Init(ref viewportimg);
+
+            this.MouseMove += new MouseEventHandler(MainPage_MouseMove);
+            this.MouseLeftButtonDown += new MouseButtonEventHandler(MainPage_MouseLeftButtonDown);
+            this.MouseLeftButtonUp += new MouseButtonEventHandler(MainPage_MouseLeftButtonUp);
+            this.KeyDown += new KeyEventHandler(MainPage_KeyDown);
+            this.KeyUp += new KeyEventHandler(MainPage_KeyUp);
+            viewportimg.Cursor = Cursors.None;
+
+            CompositionTarget.Rendering += new EventHandler(Page_CompositionTarget_Rendering);
+            this.Focus();
+            this.CaptureMouse();
+        }
+
+        void MainPage_KeyUp(object sender, KeyEventArgs e)
+        {
+            _editor.editinputkeyup(false, false, e.Key);
+            e.Handled = true;
+        }
+
+        void MainPage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _editor.editinputkeyup(false, true, Key.None);
+        }
+
+        void MainPage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _editor.editinputkey(false, true, Key.None);
+        }
+
+        void MainPage_KeyDown(object sender, KeyEventArgs e)
+        {
+            _editor.editinputkey(false, false, e.Key);
+            e.Handled = true;
+        }
+
+        void MainPage_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(viewportimg);
+            _editor.editinputmouse(p.X, p.Y);
+        }
+
+        void Page_CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            _editor.Frame();
+        }
+    }
+}
--- trunk/buildlite/MainPage.xaml.cs
+++ trunk/buildlite/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿#if !WINDOWS_PHONE
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,99 +9,28 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
-#else
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
-#endif
 
+using build;
+using Editor;
 namespace buildlite
 {
-#if WINDOWS_PHONE
-    public partial class MainPage : PhoneApplicationPage
-#else
     public partial class MainPage : UserControl
-#endif
     {
-        BuildEditor _editor;
+        EditorPage editorPage = new EditorPage();
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        void LaunchEditor(object sender, EventArgs e)
+        {
+            LayoutRoot.Children.Add(editorPage);
+        }
+
         void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            _editor = new BuildEditor();
-
             
-
-
-            System.Windows.Interop.SilverlightHost host = Application.Current.Host;
-            // The Settings object, which represents Web browser settings.
-            System.Windows.Interop.Settings settings = host.Settings;
-
-            // Read/write properties of the Settings object.
-            settings.EnableFrameRateCounter = true;
-            //        settings.EnableRedrawRegions = true;
-            settings.MaxFrameRate = 60;
-
-            _editor.Init( ref viewportimg );
-
-            this.MouseMove += new MouseEventHandler(MainPage_MouseMove);
-            this.MouseLeftButtonDown += new MouseButtonEventHandler(MainPage_MouseLeftButtonDown);
-            this.MouseLeftButtonUp += new MouseButtonEventHandler(MainPage_MouseLeftButtonUp);
-            this.KeyDown += new KeyEventHandler(MainPage_KeyDown);
-            this.KeyUp += new KeyEventHandler(MainPage_KeyUp);
-            viewportimg.Cursor = Cursors.None;
-
-            CompositionTarget.Rendering += new EventHandler(Page_CompositionTarget_Rendering);
-            this.Focus();
-            this.CaptureMouse();
         }
-
-        void MainPage_KeyUp(object sender, KeyEventArgs e)
-        {
-            _editor.editinputkeyup(false, false, e.Key);
-            e.Handled = true;
-        }
-
-        void MainPage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            _editor.editinputkeyup(false, true, Key.None);
-        }
-
-        void MainPage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _editor.editinputkey(false, true, Key.None);
-        }
-
-        void MainPage_KeyDown(object sender, KeyEventArgs e)
-        {
-            _editor.editinputkey(false, false, e.Key);
-            e.Handled = true;
-        }
-
-        void MainPage_MouseMove(object sender, MouseEventArgs e)
-        {
-            Point p = e.GetPosition(viewportimg);
-            _editor.editinputmouse(p.X, p.Y);
-        }
-
-        void Page_CompositionTarget_Rendering(object sender, EventArgs e)
-        {
-            _editor.Frame();
-        }
-
-
     }
 }
