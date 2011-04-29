14bf7f5d8c2dceeb5460a3a7978c08a719438f3e
justinmarshall20@gmail.com <justinmarshall20@gmail.com@ff800d11-3098-c92b-a8d1-3816c941664f>
2011-04-29 01:15:39 +0000

    EditArt can now import and export tiles(can't save to art files yet).

--- trunk/editart/EditartPage.xaml.cs
+++ trunk/editart/EditartPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Media.Imaging;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -10,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using ImageExportLib.Internals;
+using build;
 
 namespace editart
 {
@@ -28,15 +31,81 @@ namespace editart
 
         void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            viewportimg.Cursor = Cursors.None;
-
             editart.Init(ref viewportimg);
 
             // Game loop.
             CompositionTarget.Rendering += new EventHandler(Page_CompositionTarget_Rendering);
         }
 
-        
+        void ExportTileClick(object sender, EventArgs e)
+        {
+            
+            int width = Engine.tilesizx[editart.SelectedArtTile];
+            int height = Engine.tilesizy[editart.SelectedArtTile];
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.DefaultExt = ".png";
+            dialog.Filter = "PNG File|*.png";
+            dialog.FilterIndex = 1;
+
+            if (width <= 0 || height <= 0 || Engine.waloff[editart.SelectedArtTile].memory == null)
+            {
+                MessageBox.Show("You can't export a tile that doesn't have any image data");
+                return;
+            }
+
+            if (!dialog.ShowDialog().Value)
+                return;
+
+            byte[] imagedata = editart.ConvertTileToImage(editart.SelectedArtTile, width, height);
+
+            System.IO.Stream file = dialog.OpenFile();
+            System.IO.MemoryStream png = (System.IO.MemoryStream)PngEncoder.Encode(imagedata, width, height);
+
+            file.Write( png.ToArray(), 0, (int)png.Length );
+            file.Close();
+
+            png.Dispose();
+        }
+
+        void ReplaceTileClick(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "PNG File|*.png";
+            dialog.FilterIndex = 1;
+
+            if (dialog.ShowDialog().Value == false)
+                return;
+
+            System.IO.Stream fileStream = dialog.File.OpenRead();
+
+            // This is kind of a long way to do this, but doing so we can properly load bmp and pngs,
+            // without any extra code, basically we create a bitmapimage, than we create a WriteableBitmap,
+            // we have to do it that way because writeablebitmap's constructor doesn't take one param,
+            // and writeablebitmap is the only way to get the pixel data out of our bitmapimage.
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.SetSource(fileStream);
+            
+            WriteableBitmap bitmapwrite = new WriteableBitmap(bitmap);
+            if (bitmapwrite.PixelWidth <= 320 && bitmapwrite.PixelHeight <= 200)
+            {
+                editart.ReplaceCurrentTile(bitmapwrite.Pixels, bitmapwrite.PixelWidth, bitmapwrite.PixelHeight);
+            }
+            else
+            {
+                MessageBox.Show("Image size must be less than 320x200!");
+            }
+
+            // Close the opened stream.
+            fileStream.Dispose();
+
+            // Null out the bitmapmaps we had to create.
+            bitmap = null;
+            bitmapwrite = null;
+
+            // Force the gc to reclaim any unused memory.
+            GC.Collect();
+        }
 
         void MainPage_KeyUp(object sender, KeyEventArgs e)
         {
@@ -52,6 +121,16 @@ namespace editart
 
         void Page_CompositionTarget_Rendering(object sender, EventArgs e)
         {
+            if (editart.DrawTileEditMenu && edittilemenu.Visibility == System.Windows.Visibility.Collapsed)
+            {
+                edittilemenu.Visibility = System.Windows.Visibility.Visible;
+                viewportimg.Cursor = Cursors.Arrow;
+            }
+            else if (!editart.DrawTileEditMenu && edittilemenu.Visibility == System.Windows.Visibility.Visible)
+            {
+                edittilemenu.Visibility = System.Windows.Visibility.Collapsed;
+                viewportimg.Cursor = Cursors.None;
+            }
             editart.Frame();
         }
     }
